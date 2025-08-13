7786d37560aeccf55bb101ee92fbca940e6c6487
GunvaldAI <vm.kuitunen@gmail.com>
2025-08-13 01:08:14 -0700

    Delete backend/db.js

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-// Prisma client singleton
-
-const { PrismaClient } = require('@prisma/client');
-
-// Create a single PrismaClient instance to be shared across modules.
-const prisma = new PrismaClient();
-
-// Export the instance for use in routes and services.
-module.exports = prisma;
